e65d407339d8c018358d0919c2702a71c18398c0
omar Fonseca <fonsecaom79@gmail.com>
2023-03-19 17:20:18 -0500

    subiendo patients

--- src/maestros/patients/dto/create-patient.dto.ts
+++ src/maestros/patients/dto/create-patient.dto.ts
@@ -1,1 +1,56 @@
-export class CreatePatientDto {}
+import { IsString, IsNumber } from 'class-validator';
+
+export class CreatePatientDto {
+
+        readonly id: string;
+        @IsString()
+        readonly IdPaciente: number;
+
+        @IsNumber()
+        readonly TipoPaciente: number;
+
+        @IsString()
+        readonly edad:string;
+
+        @IsNumber()
+        readonly razaId:number;
+
+        @IsNumber()
+        readonly religionId:number;
+
+        @IsString()
+        readonly areaLaboral:string;
+
+        @IsString()
+        readonly ocupacion:string;
+
+        @IsString()
+        readonly codigoHC:string;
+
+        @IsString()
+        readonly codigoHcAnterior:string;
+
+        @IsString()
+        readonly lugarProcedencia:string;
+
+        @IsString()
+        readonly fechaIngreso:string;
+
+        @IsNumber()
+        readonly estado:number;
+
+        @IsString()
+        readonly usuarioCreacion:string;
+
+        @IsString()
+        readonly createAd: string;
+
+        @IsString()
+        readonly ubicacionHc:string;
+
+        @IsString()
+        readonly situacion:string;
+
+        @IsString()
+        readonly fechaCreacionHc:string;
+}
--- src/maestros/patients/dto/update-patient.dto.ts
+++ src/maestros/patients/dto/update-patient.dto.ts
@@ -1,4 +1,78 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
 import { CreatePatientDto } from './create-patient.dto';
 
-export class UpdatePatientDto extends PartialType(CreatePatientDto) {}
+export class UpdatePatientDto extends PartialType(CreatePatientDto) {
+        @IsString()
+        @IsUUID()
+        @IsOptional()
+        readonly id: string;
+
+        @IsNumber()
+        @IsOptional()
+        readonly IdPaciente: number;
+
+        @IsNumber()
+        @IsOptional()
+        readonly TipoPaciente: number;
+
+        @IsString()
+        @IsOptional()
+        readonly edad:string;
+
+        @IsNumber()
+        @IsOptional()
+        readonly razaId?:number;
+
+        @IsNumber()
+        @IsOptional()
+        readonly religionId?:number;
+
+        @IsString()
+        @IsOptional()
+        readonly areaLaboral?:string;
+
+        @IsString()
+        @IsOptional()
+        readonly ocupacion?:string;
+
+        @IsString()
+        @IsOptional()
+        readonly codigoHC?:string;
+
+        @IsString()
+        @IsOptional()
+        readonly codigoHcAnterior?:string;
+
+        @IsString()
+        @IsOptional()
+        readonly lugarProcedencia:string;
+
+        @IsString()
+        @IsOptional()
+        readonly fechaIngreso:string;
+
+        @IsNumber()
+        @IsOptional()
+        readonly estado:number;
+
+        @IsString()
+        @IsOptional()
+        readonly usuarioCreacion:string;
+
+        @IsString()
+        @IsOptional()
+        readonly createAd: string;
+
+        @IsString()
+        @IsOptional()
+        readonly ubicacionHc:string;
+
+        @IsString()
+        @IsOptional()
+        readonly situacion:string;
+
+        @IsString()
+        @IsOptional()
+        readonly fechaCreacionHc:string;
+}

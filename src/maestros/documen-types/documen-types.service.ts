1ee8061f251f3a26c8aebccbb537aeded6ec57ac
omar Fonseca <fonsecaom79@gmail.com>
2023-03-19 13:49:00 -0500

    subiendo cambios

--- src/maestros/documen-types/documen-types.service.ts
+++ src/maestros/documen-types/documen-types.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDocumenTypeDto } from './dto/create-documen-type.dto';
 import { UpdateDocumenTypeDto } from './dto/update-documen-type.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class DocumenTypesService {
-  
+  private documentTypes: CreateDocumenTypeDto[] = [
+    {
+      id:uuid(),
+      documentId:"MAS01",
+      name:"MASCULINO",
+      description:"género masculino",
+      constraint:"M"
+    },
+    {
+      id:uuid(),
+      documentId:"FEM01",
+      name:"FEMENINO",
+      description:"género femenino",
+      constraint:"F"
+    }
+  ]
   create(createDocumenTypeDto: CreateDocumenTypeDto) {
     return 'This action adds a new documenType';
   }
